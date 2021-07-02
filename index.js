4837a165ed67d065e71489d47739fc54c531ffd2
Gowtham Chandrasekar <gowthamtrm01@gamil.com>
2021-07-02 13:39:44 +0530

    post-reccipe

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express, { request, response } from "express";
 import mongoose from "mongoose";
 import recipeRouter from './routers/recipes.js';
 import { router } from './routers/users.js';
+import bcrypt from 'bcrypt';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 
-const url = "mongodb+srv://gowtham:Saipallavi@0102@cluster0.wbk9z.mongodb.net/movieData";
+const url = process.env.MONGO_URL || "mongodb://localhost/movieData";
 
 mongoose.connect(url, { useNewUrlParser: true });
 
@@ -20,5 +21,22 @@ app.use('/recipes', recipeRouter);
 app.get('/', (request, response) => {
     response.send("Welcome to users");
 })
+
+
+
+async function getHash() {
+    const password = 'password@123';
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt)
+    console.log(salt, passwordHash);
+    const match = await bcrypt.compare(password, passwordHash);
+    if (!match) {
+        console.log('failed')
+    } else {
+        console.log('successfully login')
+    }
+}
+
+getHash();
 
 app.listen(PORT, () => console.log(`Server was started ${PORT}`));
--- routers/recipes.js
+++ routers/recipes.js
@@ -1,10 +1,22 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import { Recipes } from '../models/recipes.js';
 const recipeRouter = express.Router();
 
 recipeRouter.get('/', async (request, response) => {
     const recipes = await Recipes.find();
     response.send(recipes);
+})
+
+recipeRouter.post('/', async (request, response) => {
+    const addRecipe = request.body;
+    const recipe = new Recipes(addRecipe);
+    try {
+        const newRecipe = await recipe.save();
+        response.send(newRecipe);
+    } catch (err) {
+        response.status(500);
+        response.send(err);
+    }
 })
 
 export default recipeRouter;
